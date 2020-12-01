efb55fd7aed8ef74237190bbba5dc751358505db
Erwan Béguin <erwan.beguin@gmail.com>
2020-12-01 08:37:21 +0100

    Updates

---
+++ precache-manifest.21ea75f5de8f8d849d84f2deffd3bcfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f8d3ddecd30e2858a363b6f72dda38",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "2c1b885b2b66e818a676",
+    "url": "/dashboard/static/js/2.13e337aa.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/dashboard/static/js/2.13e337aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762da2b089f074d69f92",
+    "url": "/dashboard/static/js/main.4e856107.chunk.js"
+  },
+  {
+    "revision": "9b92608365be5bd363dd",
+    "url": "/dashboard/static/js/runtime-main.9659c3b8.js"
+  }
+]);
